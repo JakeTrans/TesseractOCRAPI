2e5353fb7e6500d97ca8d4e7ccf37b932d31332a
Jake Trans <Jake_Damner@yahoo.co.uk>
2024-03-31 19:11:14 +0100

    unauthorised message setup

--- Controllers/OCRController.cs
+++ Controllers/OCRController.cs
@@ -5,30 +5,42 @@ using SixLabors.ImageSharp.Formats;
 using System.Runtime.Intrinsics.X86;
 using System.Text;
 using TesseractOCRPlugin;
+using Microsoft.AspNetCore.DataProtection.KeyManagement;
+using System.Net;
+using System.Web.Http;
 
 namespace TesseractOCRAPI.Controllers
 {
     [ApiController]
-    [Route("[controller]")]
+    [Microsoft.AspNetCore.Mvc.Route("[controller]")]
     public class OCRController : ControllerBase
     {
         private readonly ILogger<OCRController> _logger;
 
-        public OCRController(ILogger<OCRController> logger)
+        private readonly IApiKeyValidation _apiKeyValidation;
+
+        public OCRController(ILogger<OCRController> logger, IApiKeyValidation apiKeyValidation)
         {
             _logger = logger;
+            _apiKeyValidation = apiKeyValidation;
         }
 
-        [HttpGet(Name = "GetOCRResults")]
-        public IEnumerable<OCResults> Get(string text)
+        [Microsoft.AspNetCore.Mvc.HttpGet(Name = "GetOCRResultsKey")]
+        public IEnumerable<OCResults> Get(string text, string apiKey)
         {
+            bool isValid = _apiKeyValidation.IsValidApiKey(apiKey);
+            if (!isValid)
+            {
+                throw new HttpResponseException(HttpStatusCode.Unauthorized);
+            }
+
             return Enumerable.Range(1, 1).Select(index => new OCResults
             {
                 Text = text
             }).ToArray();
         }
 
-        [HttpPost]
+        [Microsoft.AspNetCore.Mvc.HttpPost]
         public async Task<IActionResult> UploadImage(IFormFile file)
         {
             string text = "";
